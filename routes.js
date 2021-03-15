94617107243fa913f4f5764900a6b4d79a0b5a66
Pedro Matheus <pedromatheusduarte@gmail.com>
2021-03-15 15:19:49 -0300

    [alter][telas][routas]

--- routes.js
+++ routes.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const express = require('express');
 const PreferenceController = require('./src/controller/PreferenceController');
 const router = express.Router();
@@ -12,6 +13,18 @@ router.get('/', function (req, res) {
 router.get('/detail', function (req, res) {
     res.render('detail', req.query);
 });
+
+router.get('/success' ,function (req, res) {
+    res.render('success', req.query)
+})
+
+router.get('/pending' ,function (req, res) {
+    res.render('pending', req.query)
+})
+
+router.get('/rejected' ,function (req, res) {
+    res.render('rejected', req.query)
+})
 
 router.post('/preference', PreferenceController.preference)
 
--- src/controller/PreferenceController.js
+++ src/controller/PreferenceController.js
@@ -4,25 +4,58 @@ const mercadopago = require('mercadopago');
 
 // Configura credenciais
 mercadopago.configure({
-  access_token: process.env.PROD_ACCESS_TOKEN || "TEST-3221234070031413-030323-7a90077e2ebdf20f2112696754256ca9-256917392"
+  integrator_id: "dev_24c65fb163bf11ea96500242ac130004",
+  access_token: process.env.PROD_ACCESS_TOKEN || "APP_USR-334491433003961-030821-12d7475807d694b645722c1946d5ce5a-725736327"
 });
 
 
 module.exports = {
 
   async preference(req, res) {
-    const { title, price, unit } = req.query
+    const { title, price, unit ,img} = req.query
     // Cria um objeto de preferência
 
     let preference = {
-      items: [
-        {
+        items: [{
+          id: "1234",
           title,
-          quantity: parseInt(unit),
+          discription: "Celular de Tienda e-commerce",
+          picture_url : img,
           unit_price: Number(price),
-
-        }
-      ]
+          quantity: parseInt(unit),
+        }],
+        external_reference: "pedromatheusduarte@gmail.com",
+        payer: {
+          name: "Lalo",
+          surname: "Landa",
+          email: "test_user_92801501@testuser.com",
+          date_created: new Date().toISOString(),
+          phone: {
+            area_code: "55",
+            number: 985298743
+          },
+    
+          address: {
+            street_name: "Insurgentes Sur",
+            street_number: 1602,
+            zip_code: "78134-190"
+          }
+        },
+        payment_methods:{
+          excluded_payment_types:[
+            {
+              id:"amex"
+            }
+          ],
+          installments: 6
+        },
+        notification_url: "https://webhook.site/c7d600eb-99c9-4c92-a8ab-29521a699356",
+        back_urls: {
+          success: "http://localhost:3000/success",
+          failure: "http://localhost:3000/failure",
+          pending: "http://localhost:3000/pending"
+        },
+        auto_return: 'approved',
     };
 
     mercadopago.preferences.create(preference)
